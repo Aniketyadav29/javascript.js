73391b6866022371da70c2c79518c46f127af5d2
Aniket Yadav <anikety7905@gmail.com>
2026-01-22 23:20:40 +0530

    Update First.js

--- First.js
+++ First.js
@@ -4,3 +4,6 @@ function greet(name) {
     return `Hello, ${name}!`;
 }
 
+// Variables in Java Script :- Variables are containers for storing data values. In JavaScript, we use the var, let, and const keywords to declare variables.//
+
+
