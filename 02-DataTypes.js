3963dd708a680fbee7279305ebc400485c941859
Aniket Yadav <anikety7905@gmail.com>
2026-01-25 23:19:40 +0530

    Update 02-DataTypes.js

--- 02-DataTypes.js
+++ 02-DataTypes.js
@@ -1,1 +1,3 @@
-
+// Data Types in JavaScript:- 1. Primitive Data Types 2. Reference Data Types
+// 1. Primitive Data Types:- String, Number, Boolean, Null, Undefined, Symbol
+// 2. Reference Data Types:- Arrays, Object Literals, Functions, Dates
