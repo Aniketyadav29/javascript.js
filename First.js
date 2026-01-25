f6f435ddc270ff213499f7848167459e96257441
Aniket Yadav <anikety7905@gmail.com>
2026-01-25 23:01:50 +0530

    Remove duplicate comment on JavaScript variables
    Removed duplicate comment about variables in JavaScript.

--- First.js
+++ First.js
@@ -10,7 +10,6 @@ function addthreenumbers(a, b, c) {
 console.log(addthreenumbers(1, 2, 3));
 
 // Variables in Java Script :- Variables are containers for storing data values. In JavaScript, we use the var, let, and const keywords to declare variables.//
-// Variables in Java Script :- Variables are containers for storing data values. In JavaScript, we use the var, let, and const keywords to declare variables.//
 age=24;
 console.log(age);
 
