df94b64aa9623bf0733c08522e428f00c898edf8
Aniket Yadav <anikety7905@gmail.com>
2026-01-25 22:32:14 +0530

    Add function to sum three numbers and log results
    Added console logs and a function to add three numbers.

--- First.js
+++ First.js
@@ -4,6 +4,11 @@ function greet(name) {
     return `Hello, ${name}!`;
 }
 
+console.log('hello world');
+function addthreenumbers(a, b, c) {
+    return a + b + c;
+console.log(addthreenumbers(1, 2, 3));
+
 // Variables in Java Script :- Variables are containers for storing data values. In JavaScript, we use the var, let, and const keywords to declare variables.//
 
 
