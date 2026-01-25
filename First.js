e2f4a031eb885794467ef5876481d426a4ee5703
Aniket Yadav <anikety7905@gmail.com>
2026-01-25 23:01:06 +0530

    Correct variable declarations and console log statements
    Fixed typos and improved variable declarations in First.js.

--- First.js
+++ First.js
@@ -10,5 +10,20 @@ function addthreenumbers(a, b, c) {
 console.log(addthreenumbers(1, 2, 3));
 
 // Variables in Java Script :- Variables are containers for storing data values. In JavaScript, we use the var, let, and const keywords to declare variables.//
+// Variables in Java Script :- Variables are containers for storing data values. In JavaScript, we use the var, let, and const keywords to declare variables.//
+age=24;
+console.log(age);
+
+name="Aniket Yadav";
+consile.log("name");
+
+let a = 10; // let is used to declare a variable that can be reassigned
+const b = 20; // const is used to declare a variable that cannot be reassigned
+console.log(a)
+
+// let 8harry = "harry" // Not allowed this will throw an error
+// let var = 45 // Not allowed this will throw an error
+
+// var:- var is a global variable and can be reassigned.
 
 
