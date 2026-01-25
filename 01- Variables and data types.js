f8ce6cd2f7e6abc8670e51c696ef5380af4f0532
Aniket Yadav <anikety7905@gmail.com>
2026-01-25 22:46:57 +0530

    Update 01- Variables and data types.js

--- 01- Variables and data types.js
+++ 01- Variables and data types.js
@@ -8,3 +8,6 @@ consile.log("name");
 let a = 10; // let is used to declare a variable that can be reassigned
 const b = 20; // const is used to declare a variable that cannot be reassigned
 console.log(a)
+
+// let 8harry = "harry" // Not allowed this will throw an error
+// let var = 45 // Not allowed this will throw an error
