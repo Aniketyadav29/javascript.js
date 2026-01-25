2cd5c2d50d3e0ae9511caf9b3f49552c5c3b551f
Aniket Yadav <anikety7905@gmail.com>
2026-01-25 22:31:12 +0530

    Update 01- Variables and data types.js

--- 01- Variables and data types.js
+++ 01- Variables and data types.js
@@ -1,1 +1,6 @@
 // Variables in Java Script :- Variables are containers for storing data values. In JavaScript, we use the var, let, and const keywords to declare variables.//
+age=24;
+console.log(age);
+
+name="Aniket Yadav";
+consile.log("name");
