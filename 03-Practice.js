9705ad6ff9bec38c3cc33b879d6777d1d612783a
Aniket Yadav <anikety7905@gmail.com>
2026-01-26 23:23:33 +0530

    Rename 03-Practicejs to 03-Practice.js

---
+++ 03-Practice.js
@@ -0,0 +1,30 @@
+// practice set:-
+// Q1:- create a variable of type string and try to add a number to it.
+
+let m="Harry"
+let n=6
+add= m+n
+console.log(add)
+
+// Q2:- use typeof operator to find the datatype of the string in last question.
+
+console.log(typeof add)
+console.log(typeof m)
+console.log(typeof n)
+
+// Q3:- create a const object in javascript can you change it to hold a number later?
+
+const a1={
+  name:"Aniket",
+  section:1,
+  isPrincipal:false
+  // isPrincipal=true // this will throw an error because const object can not be changed.
+}
+// a1="harry" // this will throw an error because const object can not be changed."
+
+// Q4:- try to add a new key to the const object in problem 3 were you able to do it?
+
+a1['friend']="shubham" // this will not throw an error because we are not changing the object but adding a new key to it.
+
+console.log(a1)
+
