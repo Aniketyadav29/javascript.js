90eae53c9a347f2389f50e57a9b16cae0ffdecef
Aniket Yadav <anikety7905@gmail.com>
2026-01-26 23:21:15 +0530

    Enhance data types explanation in 02-DataTypes.js
    Added BigInt and Symbol data types to JavaScript example.

--- 02-DataTypes.js
+++ 02-DataTypes.js
@@ -1,3 +1,16 @@
 // Data Types in JavaScript:- 1. Primitive Data Types 2. Reference Data Types
 // 1. Primitive Data Types:- String, Number, Boolean, Null, Undefined, Symbol
 // 2. Reference Data Types:- Arrays, Object Literals, Functions, Dates
+// NN BB SS U:- Null, Number, Boolean, BigInt, String, Symbol, Undefined.
+
+let a=null;
+let b=345; 
+let c=true; // can also be false
+let d=BigInt("567")
+let e="harry"
+let f=Symbol("I am a nice symbol")
+let g=undefined  // let g; // both are same
+console.log(a,b,c,d,e,f,g) // null, 345, true, 570n, harry, Symbol(I am a nice symbol), undefined
+console.log(typeof d)
+console.log(typeof c)  
+
