8f7108d8f081ae7e20b5975321f0b96fc07a085f
Aniket Yadav <anikety7905@gmail.com>
2026-01-26 23:25:21 +0530

    Document JavaScript operators with examples
    Added comments explaining various operators in JavaScript and provided examples of arithmetic operations.

--- 04-OPerator.js
+++ 04-OPerator.js
@@ -1,1 +1,49 @@
+// Operators in JavaScript:- Operators are used to perform operations on variables and values.
+// 1. Arithmetic Operators:- +, -, *, /, %, ++, --, **
+// 2. Assignment Operators:- =, +=, -=, *=, /=, %=, **=
+// 3. Comparison Operators:- ==, ===, !=, !==, >, <, >=, <=
+// 4. Logical Operators:- &&, ||, !
+// 5. Bitwise Operators:- &, |, ^, ~, <<, >>, >>>
+// 6. Ternary Operator:- condition ? expr1 : expr2
+// 7. Type Operators:- typeof, instanceof
+// 8. String Operators:- +, +=
+// 9. Comma Operator:- ,
+// 10. Unary Operators:- delete, void, typeof, +, -, ~, !
+// 11. Relational Operators:- in, instanceof
+// 12. Conditional (Ternary) Operator:- condition ? expr1 : expr2
+// 13. Assignment Operators:- =, +=, -=, *=, /=, %=, <<=, >>=, >>>=, &=, ^=, |=
+// 14. Spread Operator:- ...
+// 15. Rest Operator:- ...
+// 16. Destructuring Assignment:- [] and {}
+// 17. Optional Chaining Operator:- ?.
+// 18. Nullish Coalescing Operator:- ??
+// 19. Logical Nullish Assignment:- ??=
+// 20. Logical AND Assignment:- &&=
+// 21. Logical OR Assignment:- ||=
+// 22. Exponentiation Assignment:- **=
+// 23. Exponentiation Operator:- **
+// 24. Remainder Operator:- %
+// 25. Increment Operator:- ++
+// 26. Decrement Operator:- --
+// 27. Equality Operator:- ==
+// 28. Strict Equality Operator:- ===
+// 29. Inequality Operator:- !=
+// 30. Strict Inequality Operator:- !==
+// 31. Greater Than Operator:- >
+// 32. Greater Than or Equal To Operator:- >=
+// 33. Less Than Operator:- <
+// 34. Less Than or Equal To Operator:- <=
 
+// 79;
+// "harry bhai"
+console.log("Operators in JS")
+// Arithmetic Operators:- +, -, *, /, %, ++, --, **
+let a2=45;
+let b2=4;
+console.log("a + b =",a2+b2)
+console.log("a - b =",a2-b2)
+console.log("a / b =",a2/b2)
+console.log("a ** b =",a2**b2)
+console.log("a % b =",a2%b2) // moduolo operator:- gives the remainder of the division.
+console.log("++a =",++a2) // increment operator:- increases the value of the variable by 1.
+console.log("a++ =",a2++) // increment operator:- increases the value of the variable by 1.
