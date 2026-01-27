e75c8eeae46591ebe547fc9e1edde64b1f17c6d8
Aniket Yadav <anikety7905@gmail.com>
2026-01-27 22:58:48 +0530

    Update 05-Conditional S.js

--- 05-Conditional S.js
+++ 05-Conditional S.js
@@ -2,5 +2,13 @@
 
 //if else statement:- if the condition is true then the code inside the if block will be executed. if the condition is false then the code inside the else block will be executed.
 // prompt:- prompt is used to take input from the user.
-let age=prompt("Enter your age")
+// let age=prompt("Enter your age")
 // Alert:- alert is used to display a message to the user.
+let a =prompt("Hey whats your age?")
+a=Number.parseInt(a) // converting the string to a number.
+if(a>0){
+  alert("This is a valid age")
+}
+else{
+    alert("This is an invalid age")
+}
