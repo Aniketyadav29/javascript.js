53aeda43f23e1d6d21cf173ef5cd0de0db3fb57f
Aniket Yadav <anikety7905@gmail.com>
2026-01-27 22:58:08 +0530

    Add comments for conditional expressions and prompt usage
    Added comments explaining conditional expressions and user input.

--- 05-Conditional S.js
+++ 05-Conditional S.js
@@ -1,1 +1,6 @@
+// Conditional Expressions:- if, else, else if, switch, ternary operator.
 
+//if else statement:- if the condition is true then the code inside the if block will be executed. if the condition is false then the code inside the else block will be executed.
+// prompt:- prompt is used to take input from the user.
+let age=prompt("Enter your age")
+// Alert:- alert is used to display a message to the user.
