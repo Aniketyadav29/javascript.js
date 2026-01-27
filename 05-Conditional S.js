199898f2260bcfa0951bcd4ad5ced65e68d710bb
Aniket Yadav <anikety7905@gmail.com>
2026-01-27 23:05:27 +0530

    Remove unnecessary comments from 05-Conditional S.js

--- 05-Conditional S.js
+++ 05-Conditional S.js
@@ -1,4 +1,4 @@
-
+// Conditional Expressions:- if, else, else if, switch, ternary operator.
 
 //if else statement:- if the condition is true then the code inside the if block will be executed. if the condition is false then the code inside the else block will be executed.
 // prompt:- prompt is used to take input from the user.
