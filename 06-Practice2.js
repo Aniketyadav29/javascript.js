5796376652b6af69ae178224d3ec0ed4cd63b92d
Aniket Yadav <anikety7905@gmail.com>
2026-01-27 23:30:21 +0530

    Implement age check between 10 and 20

--- 06-Practice2.js
+++ 06-Practice2.js
@@ -1,1 +1,9 @@
-
+// Q1:- use logical operators to find whether the age of a person lies between 10 and 20?
+let age1=prompt("What is your age?")
+age1=Number.parseInt(age1)
+if(age1>10 && age1<20){
+  console.log("Your age lies between 10 and 20")
+}
+else{
+   console.log("Your age does not lies between 10 and 20")
+}
