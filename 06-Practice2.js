05f8e5d833fc9ba4f6a2705abc551b7538fb8dc1
Aniket Yadav <anikety7905@gmail.com>
2026-01-28 23:26:51 +0530

    Implement divisibility check for 2 and 3
    Add functionality to check divisibility by 2 and 3.

--- 06-Practice2.js
+++ 06-Practice2.js
@@ -9,3 +9,13 @@ else{
 }
 
 // write a java script program to find whether a number is divisible by 2 and 3.
+let num=prompt("enter the  number")
+num=Number.parseInt(num)
+if(num%2==0 && num%3==0){
+  console.log("Your number is divisible by 2 and 3")
+  
+}
+else{
+  console.log("Your number is not divisible by 2 and 3")
+}
+  
