699573c560508a3c5f65a37979dd1a2615a19f47
Aniket Yadav <anikety7905@gmail.com>
2026-01-29 23:13:37 +0530

    Refactor loop for calculating sum of natural numbers
    Fix loop syntax and improve variable usage for clarity.

--- 07-loop.js
+++ 07-loop.js
@@ -28,6 +28,16 @@ for(let i=0;i<n;i++){
   sum+=(i+1)
 }
 console.log(" sum of first " + n + " Natural numbers is " + sum )
+// Example:-
+let sum=0
+let n=15
+// let n=prompt("Enter Your NUmber")
+// n=Number.parsingInt(n)
+for(let i=0; i<n; i++){
+   sum+=(i+1)
+}
+console.log("sum of first " + n + " Natural numbers is " + sum)
+
 
 
 for(let i=0; i<500; i++){
