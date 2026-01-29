53c4f5c4e848ee2c6e07e892995bab0035df06a7
Aniket Yadav <anikety7905@gmail.com>
2026-01-29 23:08:58 +0530

    Document JavaScript loop types and their usage
    Added explanations for different types of loops in JavaScript.

--- 07-loop.js
+++ 07-loop.js
@@ -1,1 +1,10 @@
-7
+Loops in JavaScript:- Loops are used to execute a block of code multiple times. there are three types of loops in JavaScript:- for, while, do while.
+1. for loop:- for loop is used to execute a block of code multiple times. it is used when the number of iterations is known.
+
+while loop:- while loop is used to execute a block of code multiple times. it is used when the number of iterations is not known.
+
+do while loop:- do while loop is used to execute a block of code multiple times. it is used when the number of iterations is not known. the difference between while loop and do while loop is that in do while loop the code inside the loop will be executed at least once.
+
+for of  loop:- for of loop is used to iterate over the elements of an array.
+
+for in loop:- for in loop is used to iterate over the properties of an object. 
