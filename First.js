39043f1b1777c6444f8c8d38de9fc7cd3cf78820
Aniket Yadav <anikety7905@gmail.com>
2026-01-29 23:21:03 +0530

    Update First.js

--- First.js
+++ First.js
@@ -298,6 +298,12 @@ if(num%2==0 && num%3==0){
 else{
   console.log("Your number is not divisible by 2 and 3")
 }
+
+// print "you can drive" or "you cannot drive" based on age being greater than 18 using ternary operator.
+let age=19;
+let a=age>18?"you can drive":"you cannot drive"
+console.log(a)
+console.log(age>18?"you can drive":"you cannot drive")
   
 
 
