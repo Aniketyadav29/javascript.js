3a3a89c223cd5ffbe0e4447d559d530c36558148
Aniket Yadav <anikety7905@gmail.com>
2026-01-29 23:23:02 +0530

    Update First.js

--- First.js
+++ First.js
@@ -304,6 +304,18 @@ let age=19;
 let a=age>18?"you can drive":"you cannot drive"
 console.log(a)
 console.log(age>18?"you can drive":"you cannot drive")
+
+// Loops in JavaScript:- Loops are used to execute a block of code multiple times. there are three types of loops in JavaScript:- for, while, do while.
+// 1. for loop:- for loop is used to execute a block of code multiple times. it is used when the number of iterations is known.
+
+// while loop:- while loop is used to execute a block of code multiple times. it is used when the number of iterations is not known.
+
+// do while loop:- do while loop is used to execute a block of code multiple times. it is used when the number of iterations is not known. the difference between while loop and do while loop is that in do while loop the code inside the loop will be executed at least once.
+
+// for of  loop:- for of loop is used to iterate over the elements of an array.
+
+// for in loop:- for in loop is used to iterate over the properties of an object.
+
   
 
 
