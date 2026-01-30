359b86a0e1fa3dc246547ad264d3cfac8ea21b31
Aniket Yadav <anikety7905@gmail.com>
2026-01-30 22:37:02 +0530

    Add avg function to calculate averages

--- 08-Fuction.js
+++ 08-Fuction.js
@@ -12,3 +12,16 @@ let r=50
 console.log("Average of p and q is ", avg (p,q))
 console.log("Average of q and r is ", avg(q,r))
 console.log("Average of p and r is ", avg(p,r))
+function avg(x,y){
+    console.log("Done")
+    return (x+y)/2
+}
+//Example:-
+let u=10
+let v=20
+let w=50
+console.log("Average of u and v is ", avg (u,v))
+console.log("Average of v and w is ", avg(v,w))
+console.log("Average of u and w is ", avg(u,w))
+
+// Example:-
