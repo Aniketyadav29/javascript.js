401ac17e7853a57f595907bbe9e18e0780ffa4fc
Aniket Yadav <anikety7905@gmail.com>
2026-01-30 22:35:59 +0530

    Implement average calculation function
    Added a function to calculate the average of two numbers and log the results.

--- 08-Fuction.js
+++ 08-Fuction.js
@@ -1,3 +1,14 @@
 /* Functions in JavaScript:- Functions are used to perform a specific task. they are used to make the code more readable and understandable. they are used to avoid the repetition of the code. */
 
 // Example:-
+
+function avg(x,y){
+    console.log("Done")
+    return (x+y)/2
+}
+let p=10
+let q=20
+let r=50
+console.log("Average of p and q is ", avg (p,q))
+console.log("Average of q and r is ", avg(q,r))
+console.log("Average of p and r is ", avg(p,r))
