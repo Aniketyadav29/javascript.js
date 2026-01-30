8082c936d36653c4c384a08931631b75650454c1
Aniket Yadav <anikety7905@gmail.com>
2026-01-30 22:39:18 +0530

    Implement average and sum functions with examples
    Added functions to calculate averages and sums with examples.

--- First.js
+++ First.js
@@ -318,6 +318,39 @@ console.log(age>18?"you can drive":"you cannot drive")
 
   
 
+/* Functions in JavaScript:- Functions are used to perform a specific task. they are used to make the code more readable and understandable. they are used to avoid the repetition of the code. */
 
+// Example:-
+function avg(x,y){
+    console.log("Done")
+    return (x+y)/2
+}
+let p=10
+let q=20
+let r=50
+console.log("Average of p and q is ", avg (p,q))
+console.log("Average of q and r is ", avg(q,r))
+console.log("Average of p and r is ", avg(p,r))
+
+// Example:-
+function avg(x,y){
+    console.log("Done")
+    return (x+y)/2
+}
+let u=10
+let v=20
+let w=50
+console.log("Average of u and v is ", avg (u,v))
+console.log("Average of v and w is ", avg(v,w))
+console.log("Average of u and w is ", avg(u,w))
+
+// Example:-
+const  sum=(p,q)=>{
+    return p+q
+}
+
+console.log(n)
+console.log(sum(9,7))
+console.log(sum(100,9))
 
 
