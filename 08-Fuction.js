3f141dd7937dfa6eef60b6ce4ff5eeb27b13315f
Aniket Yadav <anikety7905@gmail.com>
2026-01-30 22:35:29 +0530

    Add comment on functions in JavaScript
    Added a comment explaining the purpose of functions in JavaScript.

--- 08-Fuction.js
+++ 08-Fuction.js
@@ -1,1 +1,3 @@
+/* Functions in JavaScript:- Functions are used to perform a specific task. they are used to make the code more readable and understandable. they are used to avoid the repetition of the code. */
 
+// Example:-
