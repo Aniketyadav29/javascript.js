90edf7471909b2f07fc3c78b891b5bf807d71759
Aniket Yadav <anikety7905@gmail.com>
2026-01-31 22:45:53 +0530

    Add program to print student marks from an object

--- 09-Practice3.js
+++ 09-Practice3.js
@@ -1,1 +1,13 @@
+// Q1:- write a program to print the marks of a student in an object using for loop.
 
+let obj={
+    harry:90,
+    Aniket:99,
+    shubh:56,
+    ritika:-1,
+    Anupam:99
+        
+}
+for(let a in obj){
+     console.log("Marks of " + a +" are " + obj[a])
+}
