3e76415469f407c8511c18878df8ea52afd0bffe
Aniket Yadav <anikety7905@gmail.com>
2026-01-31 22:47:12 +0530

    Implement mean function for five numbers
    Added a function to calculate the mean of five numbers.

--- 09-Practice3.js
+++ 09-Practice3.js
@@ -21,3 +21,15 @@ while(i!=cn){
 }
 console.log("You have entered a correct number")
 
+// write a function to find mean of 5 numbers.
+function Mean(a,b,c,d,e){
+    return (a+b+c+d+e)/5
+}
+let p=1
+let b=2
+let c=3
+let d=4
+let e=5
+console.log(Mean(1,2,3,4,5))
+
+
