bd298753a02c7e0fee17373449868e2c88bd5986
Aniket Yadav <anikety7905@gmail.com>
2026-02-01 22:43:16 +0530

    Add string example with length and indexing
    Added example usage of string properties in JavaScript.

--- 10-String.js
+++ 10-String.js
@@ -1,1 +1,4 @@
 //String:- String is a sequence of characters. it is used to store and manipulate text. it is a primitive data type. it is immutable.
+let name="Aniket"
+console.log(name.length )
+console.log(name[0])
