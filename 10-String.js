b55eec05892af24fbfad5cb73fbb8d071d954e7d
Aniket Yadav <anikety7905@gmail.com>
2026-02-01 22:46:15 +0530

    Update 10-String.js

--- 10-String.js
+++ 10-String.js
@@ -2,3 +2,8 @@
 let name="Aniket"
 console.log(name.length )
 console.log(name[0])
+// Eample:-
+let friend='Prakash'
+console.log(friend.length)
+console.log(friend[0])
+// Template literals:- Template literals are used to create strings. they are used to create strings with embedded expressions. they are used to create multi-line strings. they are used to create strings with tags.
